cabf765c68f28ddc0049f969e56ec805ad9d0b75
FzFeather <74617544+FzFeather@users.noreply.github.com>
2020-11-19 00:52:18 +0800

    Minor bug fixed
    @Iamrealmmm See see if ok

--- functions/achieve_coins_rank.py
+++ functions/achieve_coins_rank.py
@@ -7,27 +7,25 @@ def __invoke__(arg):
 	if not 1 <= Rank <= 20:
 		print('輸入錯誤：等級必須為1~20!')
 		return
-		Coinslevel = [320, 432, 544, 640, 736, 832, 925, 1024, 1120, 1312, 1408, 1504, 1594, 1687, 1775, 1866, 1951, 2044, 2133, 2222]
-		coins = int(Coinslevel[Rank-1])
-		for i in range(10):
-			level = math.ceil(math.ceil(Numberofcoinneeded/coins)*40/(i+1))
-			if level <= 56:
-				if level%8 == 0:
-					level = level/8*5
-				elif level%4 == 0:
-					level = round(level/8*5)-2
-				elif (level-1)%4 == 0:
-					level = round(level/8*5)-1
-				else:
-					level = round(level/8*5)+level%8
-			elif level > 56 :
-				level -= 56
-				if level%6 == 0:
-					level = level/3+56
-				else:
-					level = (level-2)/3+58
-					print("你必須到達", level,"層",(i+1),"次")
-			else :
-				print("error")
-			if level <= 10:
-				break
+	Coinslevel = [320, 432, 544, 640, 736, 832, 925, 1024, 1120, 1312, 1408, 1504, 1594, 1687, 1775, 1866, 1951, 2044, 2133, 2222]
+	coins = int(Coinslevel[Rank-1])
+	for i in range(10):
+		level = math.ceil(math.ceil(Numberofcoinneeded/coins)*40/(i+1))
+		if level <= 56:
+			if level%8 == 0:
+				level = level/8*5
+			elif level%4 == 0:
+				level = round(level/8*5)-2
+			elif (level-1)%4 == 0:
+				level = round(level/8*5)-1
+			else:
+				level = round(level/8*5)+level%8
+		elif level > 56 :
+			level -= 56
+			if level%6 == 0:
+				level = level//3+56
+			else:
+				level = (level-2)//3+58
+		print("你必須到達", level,"層",(i+1),"次")
+		if level <= 10:
+			break
